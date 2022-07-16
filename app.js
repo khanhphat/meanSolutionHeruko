b38d39c03c1ab80d219b58b2b3f801e0a90a677a
nhkphat <nguyenhuukhanhphat@gmail.com>
2022-07-16 12:09:42 +0700

    render html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.sendFile( __dirname + 'login.html')
+})
 app.listen(process.env.PORT || 3000)
